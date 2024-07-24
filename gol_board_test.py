932f6a5c469e3aad5fb76cb4c0ea9b4e90f9c730
Lennard Wittenberg <lennwitt@freenet.de>
2024-07-24 16:28:14 +0200

    GoLPy0.3: Unit tests added

--- gol_board_test.py
+++ gol_board_test.py
@@ -16,6 +16,7 @@ class TestGameOfLife(unittest.TestCase):
         self.assertEqual(self.test_game.current_cells[2][2], 1)
         self.assertEqual(self.test_game.current_cells[5][5], 1)
 
+    """ private function removed from tests
     def test_cell_in_grid(self):
         # testing cell in grid function.
         pos1 = self.test_game.__cell_in_grid(0, 0)
@@ -29,3 +30,36 @@ class TestGameOfLife(unittest.TestCase):
         self.assertEqual(pos3, 1)
         self.assertEqual(pos4, 0)
         self.assertEqual(pos5, 0)
+    """
+
+    def test_enforce_rules(self):
+        self.test_game_neigh = Board(3, 3)
+        
+        # Initialize the specific test scenario
+        self.test_game_neigh.current_cells[1][0] = 1
+        self.test_game_neigh.current_cells[1][1] = 1
+        self.test_game_neigh.current_cells[1][2] = 1
+        
+        self.test_game_neigh.enforce_rules()
+        self.test_game_neigh.swap_cell_states()
+        
+        self.assertEqual(self.test_game_neigh.current_cells[0][1], 1)
+        self.assertEqual(self.test_game_neigh.current_cells[1][1], 1)
+        self.assertEqual(self.test_game_neigh.current_cells[2][1], 1)
+
+    def test_check_dead_life(self):
+        self.test_game_neigh = Board(3, 3)
+        
+        # Initialize the specific test scenario
+        self.test_game_neigh.current_cells[1][0] = 1
+        self.test_game_neigh.current_cells[1][1] = 1
+        self.test_game_neigh.current_cells[1][2] = 1
+        
+        self.test_game_neigh.enforce_rules()
+        self.test_game_neigh.swap_cell_states()
+        
+        result = self.test_game_neigh.check_dead_life()
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
