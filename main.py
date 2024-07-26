dc16f15bf0bb9fa2dc0af0d6afc1395b42b2c048
Lennard Wittenberg <lennwitt@freenet.de>
2024-07-26 15:40:00 +0200

    GoLPy1.3.2.3: minor canvas representation progress

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ def load_initial_configuration(board, input_file):
                 row_idx, col_idx = map(int, line.split())
                 board.current_cells[row_idx][col_idx] = 1  # Setting the cell as alive
 
+def transfer_cell_to_canvas(cells, game, win):
+    new_cells = cells.copy()
+    for i in range(game.max_rows):
+        for j in range(game.max_cols):
+            if game.current_cells[i][j] == 1:
+                    wc = Cell(i,j,i+5,j+5, win)
+                    new_cells.append(wc)
+    return new_cells
+
+
 def main():
     args = parse_args()
 
@@ -35,15 +45,18 @@ def main():
     load_initial_configuration(game, input_file)
 
     cells = []
-
+    cells = transfer_cell_to_canvas(cells, game, win) # in spirit of pure functions
+    for cell in cells:
+        cell.draw()
+    """
     c1 = Cell(0, 0, 5, 5,win)
     c1.draw()
 
-    c2 = Cell(max_p_x-5, max_p_y-5, max_p_x-1, max_p_y-1,win)
+    c2 = Cell(max_p_x-5, max_p_y-5, max_p_x, max_p_y,win)
     c2.draw()
-    
+    """
     win.wait_for_close()
-    game.run_game_logic()
+    game.run_game_logic()    
 
 if "__main__" == __name__:
     main()
--- milestones.md
+++ milestones.md
@@ -6,17 +6,18 @@
 
 ## Phase 1.1: Conversion of previous Implementation
 - [x] Convert old code from Game-of-Life c into Python
-- [ ] Attempt to refactor Maze-solver code for the Game UI
+- [x] Attempt to refactor Maze-solver code for the Game UI
 
 ## Phase 2: Basic Implementation
 - [x] Implement the game grid and rules in Python
-- [ ] Create a grid representation using Tkinter
+- [x] Create a grid representation using Tkinter
 - [x] Implement basic cell logic (alive or dead)
 
 ## Phase 3: Game Functionality
 - [x] Implement game loop to update the grid
-- [-] Add functionality to start and stop the game
-- [-] Allow users to set initial state interactively
+- [ ] Transfer Gol logic onto the canvas
+- [/] Add functionality to start and stop the game
+- [/] Allow users to set initial state interactively
 
 ## Phase 4: Enhancements
 - [-] Add functionalities to load and save game states
